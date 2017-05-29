c1d76674e22a5f6a768f2343611c3fc916cd0367
MarcMenard <marc.menard.contact@gmail.com>
2017-05-29 10:48:41 +0200

    Commit du code

--- app/src/main/java/com/example/marcmenard/applivisiteur/MainActivity.java
+++ app/src/main/java/com/example/marcmenard/applivisiteur/MainActivity.java
@@ -5,8 +5,11 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView logo_accueil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +29,12 @@ public class MainActivity extends AppCompatActivity {
         buttonPL.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, planning_general.class);
+                Intent intent = new Intent(MainActivity.this, JSON_Planning.class);
                 startActivity(intent);
             }
         });
+
+        logo_accueil = (ImageView) findViewById(R.id.logo_accueil);
+
     }
 }
